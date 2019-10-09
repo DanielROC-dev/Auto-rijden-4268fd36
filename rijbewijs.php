3b3a5d4c9af89f2dea47ada38d71d2e5a604bd56
DanielROC-dev <dkempers@roc-dev.com>
2019-10-09 09:13:56 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,9 @@
+<?php
+$line = readline("> ");
+$b = 16.499999999999999999999;
+if ($line > $b) {
+	echo "Je mag beginnen met rijlessen!";
+} else { 
+echo "Je mag nog niet beginnen met rijlessen!";
+}
+?>
